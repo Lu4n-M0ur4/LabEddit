905fc3bf05567bfd0c7e41f2e80dd99cf643bb0c
Lu4n-M0ur4 <luan.moura-rj@hotmail.com>
2024-02-02 00:02:15 -0300

    UserTest

--- tests/UserBusiness/signup.test.ts
+++ tests/UserBusiness/signup.test.ts
@@ -1,10 +1,11 @@
 import { ZodError } from "zod";
 import { UserBusiness } from "../../src/business/UserBusiness";
-import { LoginSchema } from "../../src/dtos/users/login.dto";
+
 import { HashManagerMock } from "../mocks/HashManagerMock";
 import { IdGeneratorMock } from "../mocks/IdGeneratorMock";
 import { TokenManagerMock } from "../mocks/TokenManagerMock";
 import { UserDatabaseMock } from "../mocks/UserDatabaseMock";
+import { SignupSchema } from "../../src/dtos/users/signup.dto";
 
 describe("Testes de login da UserBusiness", () => {
   const userBusiness = new UserBusiness(
@@ -13,24 +14,26 @@ describe("Testes de login da UserBusiness", () => {
     new HashManagerMock(),
     new IdGeneratorMock()
   );
-  test("deve retornar token ao se logar corretamente", async () => {
-    const input = LoginSchema.parse({
-      email: "fulano@email.com",
-      password: "fulano123",
+  test("deve retornar token ao se cadastrar corretamente", async () => {
+    const input = SignupSchema.parse({
+      name:"Luan",
+      email: "Luan@email.com",
+      password: "Luan123",
     });
 
-    const output = await userBusiness.login(input);
+    const output = await userBusiness.signup(input);
 
-    expect(output).toEqual({ token: "token-mock-fulano" });
+    expect(output).toEqual({ token: "token-mock" });
   });
 
-  test("zod retornar um erro ao se logar com parametros invalidos", async () => {
+  test("zod retornar um erro ao se cadastrar com parametros invalidos", async () => {
     expect.assertions(1)
     
     try {
-      const input = LoginSchema.parse({
+      const input = SignupSchema.parse({
+        name:"L",
         email: "fulano@email.com",
-        password: 1,
+        password: undefined,
       });
 
     } catch (error) {
